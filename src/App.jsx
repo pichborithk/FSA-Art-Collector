6e782f643805c29d3c2d55622fda96441a878100
Pichborith Kong <pichborith@gmail.com>
2023-04-05 20:06:10 -0700

    build search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import { Feature, Loading, Preview, Search, Title } from './components';
+import { flushSync } from 'react-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState('');
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='App'>
+      <Title />
+      <Search setIsLoading={setIsLoading} setSearchResults={setSearchResults} />
+      <Preview />
+      <Feature />
+      {isLoading && <Loading />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
