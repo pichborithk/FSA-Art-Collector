169f8dc7279f7e350d7e052724014eed1ea61dc3
Pichborith Kong <pichborith@gmail.com>
2023-04-06 12:33:51 -0700

    change to use sass instead of CSS

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div className='App'>
+      <>
         <Title />
         <Search
           setIsLoading={setIsLoading}
@@ -26,7 +26,7 @@ function App() {
           setIsLoading={setIsLoading}
           setSearchResults={setSearchResults}
         />
-      </div>
+      </>
       <Loading isLoading={isLoading} />
     </>
   );
